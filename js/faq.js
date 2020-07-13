caf54777085d15bd501264c3ffc81c1f224b8a02
PrototypeAcquired <66842714+PrototypeAcquired@users.noreply.github.com>
2020-07-13 12:04:45 -0700

    Update faq.js

--- js/faq.js
+++ js/faq.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-  // Check for click events on the navbar burger icon
   $(".navbar-burger").click(function () {
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
@@ -27,19 +25,12 @@ var i = 1;
 var s = i - 1;
 var index;
 function themeChanger() {
-  //var doc = document.getElementsByClassName("toggle");
-  // var doc = document.querySelectorAll('.toggle');
-  //console.log(doc.length);
   var colorArray = ["red", "blue", "purple", "green"];
   var prevColor = colorArray[s];
   var color = colorArray[i];
   $(".toggle").addClass(color).removeClass(prevColor);
   $(".faq").addClass(color).removeClass(prevColor);
-  /*
-  for (index = 0; index < doc.length; index++) { 
-    doc[index].style.backgroundColor = color[i];
-  } */
-  //doc.style.backgroundColor = color[i];
+
   i = (i + 1) % colorArray.length;
 
   if (i == 0) {
@@ -48,9 +39,5 @@ function themeChanger() {
     s = i - 1;
   }
 
-  // s = i - 1;
-  /*
-  console.log(i+"i"+color);
-  console.log(s+"s"+color);*/
 }
 setInterval(themeChanger, 3000);
